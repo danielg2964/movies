c1f9eea2e7ba0526436ab7cdc1df9dc901c08fa8
Dani <danielg2964@gmail.com>
2025-06-17 23:19:53 -0500

    add: sql script for categories

---
+++ db/categories.script.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (uuid, name)
+VALUES
+	('cd6302de-312f-43f9-82ad-bd9e5c07d184', 'Terror'),
+	('03940d82-3c48-4cb3-8559-96d48fbdb777', 'Suspenso'),
+	('caf78cec-f4f1-491a-8f12-1fe75680f0aa', 'Drama'),
+	('7b9fbc3e-a6fc-458a-9b49-d7a96ba24a02', 'Comedia');
+
