44fd8164242869a36c249cefc3e080e02844ae84
Dani <danielg2964@gmail.com>
2025-06-18 00:03:59 -0500

    use default port from process.env

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { createApp } from "#infrastructure/app.ts";
 async function main(): Promise<void> {
   const app = await createApp();
 
-  const listening = await app.listen({ port: 3000 });
+  const port = Number(process.env["PORT"]! || 4000);
+
+  const listening = await app.listen({ port: port });
 
   console.log(app.printRoutes())
   console.log(listening)
