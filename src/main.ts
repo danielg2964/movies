fa85ba3afc8b23f5f630192c3c67a568e3bd2eb8
Dani <danielg2964@gmail.com>
2025-06-18 00:11:57 -0500

    little change

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function main(): Promise<void> {
 
   const port = Number(process.env["PORT"]! || 4000);
 
-  const listening = await app.listen({ port: port });
+  const listening = await app.listen({ port: port, path: "0.0.0.0" });
 
   console.log(app.printRoutes())
   console.log(listening)
